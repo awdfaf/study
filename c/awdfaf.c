c83e857f96fe107cb02ec35474ce0a1dd8616c74
awdfaf <gunwoo1018@hanmail.net>
2020-12-30 20:58:46 +0900

    재귀함수 - 하노이의탑

--- c/awdfaf.c
+++ c/awdfaf.c
@@ -1,42 +1,18 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
 #include<math.h>
 
-
-
-int main()
-{
-    int N,M,sum=0,max=-1;
-
-    scanf("%d %d",&N,&M);
-
-    int a[N];
-
-    for(int i=0;i<N;i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    for(int i=0;i<N-2;i++)
-        for(int j=i+1;j<N-1;j++)
-            for(int k=j+1;k<N;k++)
-                {
-                    sum=a[i]+a[j]+a[k];
-                    if(sum==M)
-                    {
-                        printf("%d",sum);
-                        return 0;
-                    }
-                    else if(sum<M)
-                    {
-                        if(sum>max)
-                            max=sum;
-                    }
-                }
-    printf("%d",max);
-
-
-    return 0;
+void hanoi_tower(int n, int from, int with, int to) {
+	if (n == 1) {
+		printf("%d %d\n", from, to);
+		return;
+	}
+	hanoi_tower(n - 1, from, to, with);
+	hanoi_tower(1, from, with, to);
+	hanoi_tower(n - 1, with, from, to);
 }
-
+int main() {
+	int n;
+	scanf("%d", &n);
+    printf("%d\n", (int)pow(2,n) - 1);
+	hanoi_tower(n, 1, 2, 3);
+}
