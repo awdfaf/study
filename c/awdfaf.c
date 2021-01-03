a471ffe9e3cdd79ebcfe3d70bf5afffe5f94d9b6
awdfaf <gunwoo1018@hanmail.net>
2021-01-03 20:10:50 +0900

    브루트 포스 - 분해합

--- c/awdfaf.c
+++ c/awdfaf.c
@@ -1,18 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 #include<math.h>
 
-void hanoi_tower(int n, int from, int with, int to) {
-	if (n == 1) {
-		printf("%d %d\n", from, to);
-		return;
-	}
-	hanoi_tower(n - 1, from, to, with);
-	hanoi_tower(1, from, with, to);
-	hanoi_tower(n - 1, with, from, to);
+
+
+int main()
+{
+    int dsum=0,a=0,len=0;
+    int sum=0,check=0,ori=0,i=0;
+
+    scanf("%d",&dsum);
+    a=dsum;
+    while(1)
+    {
+        a/=10;
+        len++;
+        if(a==0)
+            break;
+    }
+    for(ori=dsum-9*len;ori<=dsum;ori++)
+    {
+        i=ori;
+        sum=ori;
+        while(1)
+        {
+            sum+=i%10;
+            i/=10;
+            if(i==0)
+                break;
+        }
+        if(sum==dsum)
+        {
+            check=1;
+            printf("%d",ori);
+            break;
+        }
+    }
+    if(check==0)
+        printf("0");
+
+    return 0;
 }
-int main() {
-	int n;
-	scanf("%d", &n);
-    printf("%d\n", (int)pow(2,n) - 1);
-	hanoi_tower(n, 1, 2, 3);
-}
+
--- python/awdfaf.py
+++ python/awdfaf.py
@@ -1,19 +1,9 @@
-import sys
-N, K=sys.stdin.readline().split()
-nums=list(map(int,input().split()))
-K=int(K)
-selected=0
-min=K-selected
-
-for i in range(0,len(nums)-2):
-    for j in range(1,len(nums)-1):
-        for k in range(2,len(nums)):
-            if(i!=j and j!=k and i!=k):
-                selected=nums[i]+nums[j]+nums[k]
-
-            if(min>=(K-selected)):
-                if((K-selected)>=0):
-                    min=K-selected
-                    selected_min=selected
-
-print(selected_min)
+n=int(input())
+x=0
+for i in range(1,n+1):
+    a=list(map(int,str(i)))
+    s=i+sum(a)
+    if(s==n):
+        x=i
+        break
+print(x)
