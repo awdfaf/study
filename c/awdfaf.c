fe8dc3d4dbc26fd94ac5f69a83231143193975fb
awdfaf <gunwoo1018@hanmail.net>
2021-01-13 21:39:50 +0900

    정렬 - 수 정렬하기1

--- c/awdfaf.c
+++ c/awdfaf.c
@@ -5,29 +5,63 @@
 #include<math.h>
 #define max(a,b) ((a)>(b)?(a):(b))
 #define minFinder(a,b) a<b?a:b
+#define SWAP(x,y,t) (t=x,x=y,y=t)
 
-
+//버블정렬
 
 int main()
 {
-    int n,tmp;
+    int i,j,tmp,num,number[1000]={0};
 
-    scanf("%d",&n);
-    int num=665,cnt=0;
-    while(cnt!=n)
+    scanf("%d",&num);
+
+    for(i=0;i<num;i++)
+        scanf("%d",&number[i]);
+    
+    for(i=num-1;i>0;i--)
     {
-        tmp=++num;
-        while(tmp)
-        {
-            if(tmp%1000==666)
-            {
-                cnt++;
-                break;
-            }
-            tmp/=10;
-        }
+        for(j=0;j<i;j++)
+            if(number[j]>number[j+1])
+                SWAP(number[j],number[j+1],tmp);
     }
-    printf("%d",num);
+    for(int i=0;i<num;i++)
+        printf("%d\n",number[i]);
+
+
+
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+// 삽입정렬
+// int main()
+// {
+//     int i,j,key,num,number[1000]={0};
+
+//     scanf("%d",&num);
+
+//     for(i=0;i<num;i++)
+//         scanf("%d",&number[i]);
+    
+//     for(i=1;i<num;i++)
+//     {
+//         key=number[i];
+//         for(j=i-1;j>=0 && number[j]>key;j--)
+//         {
+//             number[j+1]=number[j];
+//             number[j]=key;
+//         }
+//     }
+//     for(int i=0;i<num;i++)
+//         printf("%d\n",number[i]);
+
+//     return 0;
+// }
