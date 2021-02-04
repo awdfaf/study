17f732186eb81926ce1904afbeec54ce57141d55
awdfaf <gunwoo1018@hanmail.net>
2021-02-04 21:48:05 +0900

    정렬 소트인사이드(내림차순)

--- c/awdfaf.c
+++ c/awdfaf.c
@@ -1,61 +1,39 @@
-#pragma warning(disable:4996)
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<math.h>
 #define max(a,b) ((a)>(b)?(a):(b))
-#define min(a,b) a<b?a:b
+#define minFinder(a,b) a<b?a:b
 #define SWAP(x,y,t) (t=x,x=y,y=t)
 
-int func(const void* a,const void* b)
+
+int compare(const void *a,const void *b)
 {
-    int n1=*(int*)a;
-    int n2=*(int*)b;
-    if(n1<n2)
+    if(*(int *)a>*(int *)b)
         return -1;
-    else if(n1>n2)
+    else if(*(int *)a<*(int *)b)
         return 1;
     else
         return 0;
 }
-
 int main()
 {
-    int n,sum=0,x[500000],y[8001]={0};
-
-    scanf("%d",&n);
-    for(int i=0;i<n;i++)
+    int N,i=0;
+    int num[100];
+    scanf("%d",&N);
+    while(N>0)
     {
-        scanf("%d",&x[i]);
-        sum+=x[i];
-        y[x[i]+4000]++;
-    }
+        num[i]=N%10;
+        N/=10;
+        i++;
 
-    qsort(x,n,sizeof(int),func);
-
-    int flag=0,max=1,maxidx=0;
-
-    for(int i=0;i<8001;i++)
-    {
-        if(max<=y[i])
-            max=y[i];
     }
-    for(int i=0;i<8001;i++)
-    {
-        if(max==y[i])
-        {
-            maxidx=i;
-            flag++;
-        }
-        if(flag==2)
-            break;
-    }
-    printf("%.lf\n",(double)sum/(double)n);
-    printf("%d\n",x[((n-1)/2)]);
-    printf("%d\n",maxidx-4000);
-    printf("%d\n",x[n-1]-x[0]);
     
+    qsort(num,i,sizeof(int),compare);
+
+    for(int j=0;j<i;j++)
+        printf("%d",num[j]);
 
     return 0;
 }
--- python/awdfaf.py
+++ python/awdfaf.py
@@ -1,23 +1,6 @@
-def mean(v):
-    return round(sum(v)/n)
-
-def median(v):
-    if n==1 : return (v[0])
-    else : return (v[(n-1)//2])
-
-from collections import Counter
-def many_value(v):
-    if n==1 : return v[0]
-    c=Counter(v).most_common(2)
-    return (c[1][0] if c[0][1]==c[1][1] else [0][0])
-
-def min_max(v):
-    return v[n-1]-v[0]
-
-n=int(input())
-v=sorted([int(input()) for _ in range(n)])
-
-print(mean(v))
-print(median(v))
-print(many_value(v))
-print(min_max(v))
+n=list(str(input()))
+n.sort(reverse=True)
+answer = ""
+for i in n:
+    answer += i
+print(int(answer))
