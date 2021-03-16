b976d5122d59dadc6f8fede7fc17a1f2d17c6a5f
awdfaf <gunwoo1018@hanmail.net>
2021-03-16 19:25:32 +0900

    백트래킹 - n과m (4)

--- c/awdfaf.c
+++ c/awdfaf.c
@@ -8,35 +8,28 @@
 #define SWAP(x,y,t) (t=x,x=y,y=t) //정렬 매크로 함수
 #define MAX 100001
 
-int *result=NULL;
-
-void backtracking(int n,int m,int index);
-
+int arr[9]={0},visit[9]={0};
+int N,M;
+void dfs(int num,int cnt)
+{
+    if(cnt==M)
+    {
+        for(int i=0;i<M;i++)
+            printf("%d ",arr[i]);
+        printf("\n");
+        return;
+    }
+    for(int i=num;i<=N;i++)
+    {
+        visit[i]=1;
+        arr[cnt]=i;
+        dfs(i,cnt+1);
+        visit[i]=0;
+    }
+}
 int main()
 {
-    int n,m,index=0;
-    scanf("%d %d",&n,&m);
-    result=(int *)calloc(m,sizeof(int));
-    backtracking(n,m,index);
-    free(result);
+    scanf("%d %d",&N,&M);
+    dfs(1,0);
     return 0;
-}
-
-void backtracking(int n,int m,int index)
-{
-    if(m==0)
-    {
-        for(int i=0;i<index;i++)
-            printf("%d ",result[i]);
-        printf("\n");
-    }
-    else
-    {
-        for(int i=1;i<=n;i++)
-        {
-            result[index]=i;
-            backtracking(n,m-1,index+1);
-        }
-    }
-    
 }
